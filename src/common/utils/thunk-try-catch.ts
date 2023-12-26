364fdd676b8013b275f30aa5f6b88479d9791a91
telonasport99 <telonasport99@gmail.com>
2023-12-26 22:18:13 +0300

    comment js doc thunkTryCatch

--- src/common/utils/thunk-try-catch.ts
+++ src/common/utils/thunk-try-catch.ts
@@ -4,18 +4,29 @@ import { BaseThunkAPI } from '@reduxjs/toolkit/dist/createAsyncThunk';
 import { appActions } from 'app/app.reducer';
 import { BaseResponseType} from 'common/types';
 
+/**
+ * Обертывает асинхронную логику в try-catch блок и диспатчит соответствующие действия приложения
+ * @param {BaseThunkAPI<AppRootStateType, unknown, AppDispatch, null | BaseResponseType>} thunkAPI - объект, содержащий свойства и методы для работы с thunk
+ * @param {function} logic - функция, возвращающая промис с результатом асинхронной логики
+ * @returns {Promise<T | ReturnType<typeof thunkAPI.rejectWithValue>>} - промис, возвращающий либо данные типа T, либо значение, отклоненное с помощью thunkAPI.rejectWithValue
+ */
 export const thunkTryCatch = async <T>(
     thunkAPI: BaseThunkAPI<AppRootStateType, unknown, AppDispatch, null | BaseResponseType>,
     logic: () => Promise<T>
 ): Promise<T | ReturnType<typeof thunkAPI.rejectWithValue>> => {
     const { dispatch, rejectWithValue } = thunkAPI;
+    // диспатчим действие для установки статуса приложения на "loading"
     dispatch(appActions.setAppStatus({ status: "loading" }));
     try {
+        // возвращаем результат асинхронной логики
         return await logic();
     } catch (e) {
+        // обрабатываем ошибку сети сервера и диспатчим соответствующие действия
         handleServerNetworkError(e, dispatch);
+        // возвращаем значение, отклоненное с помощью thunkAPI.rejectWithValue
         return rejectWithValue(null);
     } finally {
+        // диспатчим действие для установки статуса приложения на "idle"
         dispatch(appActions.setAppStatus({ status: "idle" }));
     }
 };
