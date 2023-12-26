ef0e3b781f0b57a51fcb524b7ddf097e2539b152
telonasport99 <telonasport99@gmail.com>
2023-12-26 22:12:53 +0300

    comment js doc create-app-async-thunk

--- src/common/utils/create-app-async-thunk.ts
+++ src/common/utils/create-app-async-thunk.ts
@@ -2,6 +2,13 @@ import {createAsyncThunk} from "@reduxjs/toolkit";
 import {AppDispatch, AppRootStateType} from "app/store";
 import {BaseResponseType} from "common/types";
 
+/**
+ * Создает асинхронный thunk с помощью функции createAsyncThunk из библиотеки Redux Toolkit
+ * @param {string} type - строка, определяющая тип действия
+ * @param {function} payloadCreator - функция, возвращающая промис с результатом асинхронной логики
+ * @param {object} options - объект с типами state, dispatch и rejectValue для thunkAPI
+ * @returns {function} thunk action creator, который запускает промис-коллбэк и диспатчит действия в зависимости от результата
+ */
 export const createAppAsyncThunk = createAsyncThunk.withTypes<{
     state: AppRootStateType
     dispatch: AppDispatch
